de64ece55758b214529b2ef253b072ecd5e25001
Mar2222 <50607600+Mar2222@users.noreply.github.com>
2019-06-07 14:19:59 +1000

    Update environment.py
    contient les fonctions permettant de trouver, les voies possibles, les véhicules leaders et les véhicules followers.

--- environment.py
+++ environment.py
@@ -4,25 +4,37 @@ Created on Fri May 24 10:34:49 2019
 
 @author: mgtpe
 """
+
 import numpy as np
 import pandas as pd
 
 
 """
-fonction permettant de déterminer les voies adjacentes à la voie sur laquelle se trouve le véhicule
+fonction permettant de déterminer les voies possibles (sur lesquelles le véhicule peut aller)
+depuis une voie considérée (i.e sur laquelle se trouve le véhicule)
+
+ENTREES: 
+    road (float)= numéro de la route considérée 
+    lane (float)= numéro de la voie considérée
+    Network (Dataframe)= description du réseau (contient pour l'instant: 'road', 'lane', 'vitesse limite')
+
+SORTIE:
+    adjoining (liste)= liste des voies sur lesquelles le véhicule peut aller 
+    (la voie sur laquelle se trouve le véhicule est le dernier élément de la liste)
 """
 
 
 
 def adjoining_road(road, lane, Network):
     route=Network.loc[Network['road']==road,:]
-    droite=route.loc[route['lane']== lane+1, 'lane']   
+    droite=route.loc[route['lane']== lane+1, 'lane'] 
+    gauche= route.loc[route['lane']== lane-1, 'lane']
+    
     if droite.empty:
         d=0
     else:
-        d=1
-    
-    gauche= route.loc[route['lane']== lane-1, 'lane']
+        d=1  
+
     if gauche.empty:
         g=0
     else:
@@ -36,17 +48,28 @@ def adjoining_road(road, lane, Network):
         adjoining=[lane+1, lane]
     if d==0 and g==0 :
         adjoining=[lane]
+        
     return adjoining
 
 """
-permet de déterminer le véhicule follower dans la voie considérée
+permet de déterminer le véhicule follower dans la voie considérée, à l'instant donné
+
+ENTREES:
+    xv (float)= position de ego_vehicle au temps t
+    road (float)= numéro de la route considérée (i.e sur laquelle se trouve ego_vehicle)
+    lane (float)= numéro de la voie considérée (pas forcément celle sur laquelle se trouve ego_vehicle)
+    trajectories (DataFrame)= contient les états de tous les véhicules à chaque instant (contient: 'vehicle',
+     'time', 'acceleration', 'vitesse', 'position', 'road', 'lane')
+    t (float)= instant considéré
+
+SORTIE :
+    follower (list): liste retournant le follower sur la voie considérée
 """
 
 
 def following_vehicle(xv, road, lane, vehicles, trajectories,t):
     follower1=trajectories[(trajectories['road']==road)&(trajectories['lane']==lane)]
     follower1=follower1[(follower1['position']<xv)&(follower1['time']==t)]
- #le -0.1 a été rajouté parce que sinon des fois ego_vehicule est considéré comme son propre follower
     follower1=follower1.sort_values(by = 'position')
     follower=[]
     if follower1.empty:
@@ -57,27 +80,38 @@ def following_vehicle(xv, road, lane, vehicles, trajectories,t):
     return follower
 
 """
-fonction qui permet de déterminer les véhicules leaders considérés dans le calcul de l'accélération 
+fonction qui permet de déterminer les véhicules leaders considérés dans le calcul de l'accélération de la loi HDM
 
-entrées: 
-xv= position vehicule dans sa voie (longitudinale); Lane= voie dans laquelle on le considère; 
-road= route sur laquelle il est; cv= classe du véhicule (AV, CAV, HD CHD); vehicles= liste des véhicles se trouvant 
-avec les informations sur leur position (sur quelle route et dans quelle voie) et leur classe
+ENTREES: 
+    xv (float): position de ego_vehicle au temps t
+    road (float): numéro de la route sur laquelle se trouve ego_vehicle
+    lane float): numéro de la voie considérée (pas forcément la voie où se trouvae ego_vehicle) 
+    cv (string): classe de ego_vehicle (AV, HD)
+    vehicles (DataFrame): description des véhicules présant sur le réseau (contient:'vehicle', 'classe vehicule',
+             'type vehicule','aggressivity 0','reaction time', 'courtesy', 'respect code de la route', 'road', 
+             'lane','road turn','lane turn','position turn') 
+    trajectories (DataFrame): contient les états de tous les véhicules à chaque instant (contient: 'vehicle',
+                 'time', 'acceleration', 'vitesse', 'position', 'road', 'lane')
+    t (float): instant considéré
 
-Parametres : distances (pour le VA et le HD) pour lesquelles on prend en compte des véhicules; nombre de véhicules max 
- 
+PARAMETRES :
+    Dh: distance jusqu'à laquelle le véhicule autonome perçoit un véhicule leader (peut-être à faire dépendant de la vitesse)
+    Da: distance jusqu'à laquelle le conducteur humain perçoit un véhicule leader (peut-être à faire dépendant de la vitesse)
+    nHD: nombre max de véhicules considérés par un conducteur humain
+
+SORTIE :
+    leaders (list): liste des véhicules leaders d ego_vehicle dans la voie considérée
 """
 
 #en ne considérant que t je me retrouve avec mon ego_véhicule dans les véhicules leaders (il est son propre leader) 
 def leading_vehicles(xv, road, lane, cv, vehicles, trajectories,t):
     
-    leaders1=trajectories[(trajectories['road']==road)&(trajectories['lane']==lane)&(trajectories['time']==round(t,1))]
-    #considérée (par forcément dans la voie où se trouve notre véhicule)
-    Dh= 200 #distance considérée par un conducteur humain, peut-être une distance dépendant de la vitesse du véhicule
-    Da= 200 #distance considérée par un véhicule autonome
-    Dc= 500 # distance considérée pour la communication
-    nHD= 3  #nombre maximum de véhicules considérés par HD
+
+    Dh= 200 
+    Da= 200 
+    nHD= 3
     
+    leaders1=trajectories[(trajectories['road']==road)&(trajectories['lane']==lane)&(trajectories['time']==round(t,1))]
     leaders=[]
     
     if cv is 'HD':
@@ -123,20 +157,3 @@ def leading_vehicles(xv, road, lane, cv, vehicles, trajectories,t):
         else:
             leaders.append(leads.iloc[0]['vehicle'])
     return leaders
-"""        
-   # revoir je pense que c'est un peu n'importe quoi  
-    if cv is 'CAV':
-        leaders1=leaders1.loc[leaders1['position']>xv]
-        leaders1=leaders1.loc[leaders1['position']< Da+xv]
-        leaders1=leaders1.sort_values(by='position')
-        leaders1=vehicles.loc[vehicles['vehicle']==leaders1.iloc[0]['vehicle']]
-        leaders2=leaders1.loc[leaders1['position']>xv]
-        leaders2=leaders2.loc[leaders2['position']<Dc+xv]
-        leaders2=leaders2.loc[leaders2['communication']==True]
-        leaders3=pd.concact([leaders1,leaders2],ignore_index=True)
-        leaders=vehicles.loc[vehicles['vehicle']==leaders3['vehicle']]
-        return leaders
-    
-    else:
-        print('error class')
-"""

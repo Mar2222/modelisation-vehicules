7d93679de47e177a8867ef78ef34c207628640a6
Mar2222 <50607600+Mar2222@users.noreply.github.com>
2019-06-07 15:38:49 +1000

    Update state.py

--- state.py
+++ state.py
@@ -11,23 +11,25 @@ import pandas as pd
 fonction permettant de calculer l'accélération d'un véhicule d'après la loi de poursuite HDM
 
 ENTREES :
-  ego_vehicle (DataFrame): description du véhicule considéré (ego_vehicle) (contient: , leaders, Network, trajectories, t0, Ag
+  ego_vehicle (DataFrame): description du véhicule considéré (ego_vehicle) (contient: (contient:'vehicle', 'classe vehicule',
+             'type vehicule','aggressivity 0','reaction time', 'courtesy', 'respect code de la route', 'road', 
+             'lane','road turn','lane turn','position turn')
+  leaders (liste) : liste des ID des véhicules leaders de ego_vehicle
+  Network (DataFrame) : description du réseau (contient: 'road','lane','vitesse limite')
+  trajectories (DataFrame) : donne les états de tous les véhicules à chaque instant (contient: 'vehicle','time','position'
+              ,'vitesse','acceleration'road','lane')
+  to (float): instant considéré
+  Ag (float): aggressivité de ego_vehicle
 
-ego_vehicle:contient la vitesse en t-Tr, la position en t-Tr (longitudinale dans sa voie), la classe du véhicule (autonome ou pas),
-l'agressivité du véhicule,... 
-Network : décrit les voies et routes avec les vitesses max autorisées
-leaders: "liste" des véhicules leaders qui seront considérés dans la loi de poursuite avec les informations suivantes:
-vitesses en t-Tr, positions en t-Tr
 
-les paramères sont:
-  les paramètres de la loi HDM: a= acceleration max; delta (exposant); 
-  les para de HDM dépendant du véhicule: b=comfortable deceleration (peut-être les mêmes pour tous...),T=time headway,
-    s0=minimum gap
+PARAMETRES:
+    delta : parametre de la loi HDM, s0 : minimum gap, a_max: accélération max, T: time headway, b: comfortable deceleration
+
 """
 
 
 def HDM(ego_vehicle, leaders, Network, trajectories, t0, Ag):
-    # paramètres
+    # innitialisation des paramètres
     delta=4
     s0=2 #minimum gap
     a_max=2.2 #accélération max
@@ -52,48 +54,57 @@ def HDM(ego_vehicle, leaders, Network, trajectories, t0, Ag):
     #calcul de l'accélération idéale en free flow
     a_free= a_max*(1-(ve/v0)**delta)
     a_int=0
-    if leaders is None:
-        print('leaders is none')
-        a_mic=0
-        return a_mic
-    else:
-        if len(leaders)==0: # si il n'y a pas de véhicule leader (i.e. free flow)
-            a_mic=a_free
-            return a_mic
+    
+    if len(leaders)==0: # si il n'y a pas de véhicule leader (i.e. free flow)
+      a_mic=a_free
+      return a_mic
 
-        else:  
-            for i in range(0,len(leaders)): #calul du coefficient qui va pondérer l'impact des véhicules leaders sur la loi de poursuite. len(l) représente le nombre de leaders
-                c=c+1/((i+1)**2)
-            c=1/c
-            for j in range(0,len(leaders)):
-                vl=trajectories[(trajectories['vehicle']==leaders[j])&(trajectories['time']==round(t0,1))]
-                vl=vl.iloc[0]['vitesse']
-                xl=trajectories[(trajectories['vehicle']==leaders[j])&(trajectories['time']==round(t0,1))]
-                xl=xl.iloc[0]['position']
-                s1=s0 + max(0,ve*T+ve*(vl-ve)/(2*(a_max*b)**0.5))
-                a_int=a_int - a_max*(s1/abs(xe-xl))**2
-            a_mic=a_free + c*a_int
-            return a_mic
+    else:  # si il y a des véhicules leader
+      for i in range(0,len(leaders)): #calul du coefficient qui va pondérer l'impact des véhicules leaders sur la loi de poursuite. len(l) représente le nombre de leaders
+        c=c+1/((i+1)**2)
+      c=1/c
+      for j in range(0,len(leaders)):
+        # on cherche les vitesses et positions des véhicules leaders
+        vl=trajectories[(trajectories['vehicle']==leaders[j])&(trajectories['time']==round(t0,1))]
+        vl=vl.iloc[0]['vitesse']
+        xl=trajectories[(trajectories['vehicle']==leaders[j])&(trajectories['time']==round(t0,1))]
+        xl=xl.iloc[0]['position']
+        # on calcule l'accélération due à la présence de chaque véhicule leader considéré
+        s1=s0 + max(0,ve*T+ve*(vl-ve)/(2*(a_max*b)**0.5))
+        a_int=a_int - a_max*(s1/abs(xe-xl))**2
+      a_mic=a_free + c*a_int
+      return a_mic
 
 """
-fonction basique de vitesse où on suppose l'accélération constante entre deux pas de temps 
+fonction basique de calcul de vitesse où on suppose l'accélération constante entre deux pas de temps 
+
+ENTREES:
+  a_prece/v_prece (float): accélération/vitesse observée au pas de temps précédent
+  tp: pas de temps
 """
 
 def vitesse(a_prece,v_prece,tp):
     return a_prece*tp+v_prece
 
 """
-fonction basique de position où on suppose la vitesse constante entre deux pas de temps 
+fonction basique de calcul de la position où on suppose la vitesse constante entre deux pas de temps
+
+ENTREES:
+  v_prece/x_prece (float): vitesse/position observée au pas de temps précédent
+  tp (float): pas de temps
 """
 
 def position(v_prece,x_prece,tp):
     return v_prece*tp+x_prece
 
 """
-entrées : a_precedent: l'accélération que le véhicule avait au pas de temps précédent
-a_mic : l'accélération calculée par la loi de car-following pour le véhicule
+fonction donnant la nouvelle accélération en considérant le seuil de perception
 
-paramètre : at= seuil de perception
+ENTREES :
+  a_precedent (float): l'accélération que le véhicule avait au pas de temps précédent
+  a_mic (float): l'accélération calculée par la loi de car-following pour le véhicule
+
+PARAMETRES : at= seuil de perception
 """
 
 def perception_threshold(a_precedent, a_mic):
@@ -104,10 +115,17 @@ def perception_threshold(a_precedent, a_mic):
         return a_precedent
 
 """
-entrées: a: l'accélération que choisit d'implémenter le véhicule, cv= la classe du véhicule
-sortie: l'accélération finale (avec le bruit)
+fonction permettant d'ajouter un bruit à l'accélération (due aux erreurs de controle de la part de HD)
 
-paramètre: limportance du bruit dans l'acceleration
+ENTREES:
+  a (float): accélération que choisit d'implémenter le véhicule
+  cv (string): classe du véhicule
+
+PARAMETRES:
+  distribution aléatoire pour le bruit
+
+SORTIE: 
+  a (float): accélération réllement implémentée par le véhicule
 """
 def a_bruit(a,cv):
     if cv is 'AV' or cv is 'CAV':
